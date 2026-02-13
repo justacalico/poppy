fdfc48fc1223bba2458d811f732c38e2ee249b42
HttpAnimations <shawndouglasyt@gmail.com>
2026-02-13 00:43:21 -0700

    native sutff

--- src/components/native/NativeModal.android.tsx
+++ src/components/native/NativeModal.android.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Modal, Pressable, StyleSheet, View } from 'react-native';
+import { Modal, Pressable, StyleSheet } from 'react-native';
 
 import { useTheme } from '@/theme/ThemeProvider';
 
